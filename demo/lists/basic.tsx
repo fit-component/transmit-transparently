aaf5dc4ba9df2a414f46d9004ffb6842f2bd7993
huangziyi01 <huangziyi01@baidu.com>
2016-04-05 22:26:25 +0800

    quick push

---
+++ demo/lists/basic.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import TransmitTransparent from '../../src'
+
+@TransmitTransparent
+export default class Demo extends React.Component<any,any> {
+    render() {
+        return (
+            <div>通过 this.props.others 获取透传参数</div>
+        )
+    }
+}
