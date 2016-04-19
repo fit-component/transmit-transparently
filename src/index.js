281254e0e7988b0b52fae49412c3a8021bcea25f
huangziyi01 <huangziyi01@baidu.com>
2016-04-19 19:22:39 +0800

    quick push

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var transmit_transparently_1 = require('./transmit-transparently');
-exports.TransmitTransparently = transmit_transparently_1["default"];
-var others_1 = require('./others');
-exports.others = others_1["default"];
-exports.__esModule = true;
-exports["default"] = transmit_transparently_1["default"];
