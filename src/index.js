54063bd754b885f55400d7662bddaa2aa03e6def
huangqi03 <huangqi03@baidu.com>
2016-04-19 15:21:30 +0800

    quick push

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var transmit_transparently_1 = require('./transmit-transparently');
+exports.TransmitTransparently = transmit_transparently_1["default"];
+var others_1 = require('./others');
+exports.others = others_1["default"];
+exports.__esModule = true;
+exports["default"] = transmit_transparently_1["default"];
