885cd69343ed35a3aa732ed0a7ec1f6ba5caf968
huangziyi01 <huangziyi01@baidu.com>
2016-07-19 12:07:54 +0800

    quick push

--- src/others/index.tsx
+++ src/others/index.tsx
@@ -1,8 +1,15 @@
 import * as _ from 'lodash'
+import * as React from 'react'
+
+class HTMLAttributesClass implements React.HTMLAttributes {
+
+}
 
 export default (defaultProps: any = {}, props: any = {}, ignore?: string[])=> {
     let defaultPropsKeys: any = Object.keys(defaultProps)
     let others: any = {}
+
+    console.log(Object.keys(props))
 
     Object.keys(props).forEach((key: string)=> {
         if (!_.includes(defaultPropsKeys, key) && !_.includes(ignore, key)) {
